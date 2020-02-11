bd1bfd01f9b4b1a0fa523cb86e277c988b7f6911
jontrow1 <tro19008@byui.edu>
2020-02-11 05:41:03 -0500

    Lesson6 + updates

--- js/portal.js
+++ js/portal.js
@@ -3,3 +3,13 @@ document.getElementById('lastModified').textContent = lastModified;
 
 const options = {year: 'numeric'};
 document.getElementById('currentYear').textContent = new Date().toLocaleDateString('en-US', options);
+
+WebFont.load({
+    google: {
+      families: [
+         'Comfortaa',
+         'Indie Flower',
+         'Nunito'
+      ]
+    }
+  });
