636529ca5de10878255f34136b7ca388e999922e
jontrow1 <tro19008@byui.edu>
2020-02-18 05:51:09 -0500

    update

--- lesson07/js/lazyload.js
+++ lesson07/js/lazyload.js
@@ -3,14 +3,14 @@ const imagesToLoad = document.querySelectorAll('img[data-src]');
 
 
 const imgOptions = {
-    threshold: 0,
+    threshold: 1,
     rootMargin: "0px 0px 50px 0px"
 };
 
 const loadImages = (image) => {
     image.setAttribute('src', image.getAttribute('data-src'));
     image.onload = () => {
-        image.removeAttrribute('data-src');
+        image.removeAttribute('data-src');
     };
 };
 
