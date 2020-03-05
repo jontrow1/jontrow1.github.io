8e6755b5f192d1d2a831bdaf06d172fc3aa66b09
jontrow1 <tro19008@byui.edu>
2020-03-05 03:29:22 -0500

    updated javascript

--- lesson8/js/weather.js
+++ lesson8/js/weather.js
@@ -5,11 +5,6 @@ function toggleMenu() {
 const options = {month: 'long', day: 'numeric', year: 'numeric'};
 document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-US', options);
 
-var date = new Date();
-if (date.getDay() == 5) {
-    document.getElementById("banner").style.display="block";
-}
-
 WebFont.load({
     google: {
       families: [
