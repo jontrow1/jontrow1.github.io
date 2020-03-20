dce93a64be8051dffed539ac7cdf558f990eb692
jontrow1 <tro19008@byui.edu>
2020-03-20 04:52:57 -0400

    updated

--- lesson11/js/sodaspringsapi.js
+++ lesson11/js/sodaspringsapi.js
@@ -38,3 +38,30 @@ fetch(apiURL2)
       }
     }
 });
+
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })  
+  .then(function (jsonObject) {
+    const towns = jsonObject['towns'];
+    
+    for (let i = 0; i < towns.length; i++) {
+      if (towns[i].name == "Soda Springs") {
+        let eventul = document.createElement('ul');
+        let eventdiv = document.createElement('div');
+        for (let cont = 0; cont < towns[i].events.length; cont++) {
+          let list = document.createElement('li');
+          
+          list.setAttribute('class', 'eventlist');
+          list.textContent = towns[i].events[cont];
+          eventul.appendChild(list);
+        }
+
+        eventdiv.appendChild(eventul);
+        document.querySelector('div.eventdiv').appendChild(eventdiv);
+      }
+    }
+  });
